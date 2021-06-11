2e36d963f60f32dec916f1e81d5c104c4e78dd18
1501299026 <sinumsd98@gmail.com>
2021-06-11 08:06:11 -0700

    ADDED: check the mood

---
+++ src/test/java/com/moodanalyser/MoodAnalyserTest.java
@@ -0,0 +1,31 @@
+package com.moodanalyser;
+
+
+//import org.junit.Test;
+//import org.junit.jupiter.api.Assertions;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MoodAnalyserTest {
+  @Test
+  public void givenSadMessageShouldReturnsSad() {
+    MoodAnalyser moodAnalyser = new MoodAnalyser();
+    String mood = moodAnalyser.analyseMood("Hello am Sad mood");
+    Assertions.assertEquals("SAD", mood);
+  }
+
+  @Test
+  public void givenHappyMessageShouldReturnsHappy() {
+    MoodAnalyser moodAnalyser = new MoodAnalyser();
+    String mood = moodAnalyser.analyseMood("Hello am Happy mood");
+    Assertions.assertEquals("HAPPY", mood);
+  }
+
+  @Test
+  public void givenAnyMessageShouldReturnsHappy() {
+    MoodAnalyser moodAnalyser = new MoodAnalyser();
+    String mood = moodAnalyser.analyseMood("Hello am Dash mood");
+    Assertions.assertEquals("HAPPY", mood);
+  }
+}
